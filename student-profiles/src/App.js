f3de66e7cd5515f31cbf85ddc0ff7dbb69ef12c2
Nusorn Krachangtoy <nkrachangtoy@gmail.com>
2021-06-23 16:57:32 -0700

    create component and style folder

--- student-profiles/src/App.js
+++ student-profiles/src/App.js
@@ -1,4 +1,4 @@
-
+import './Styles/main.css';
 
 function App() {
   return (
---
+++ student-profiles/src/components/Students.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class students extends Component {
+    
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
