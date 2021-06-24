7e4e2fe2cd5a19921bc5dbee3525dde838952788
Nusorn Krachangtoy <nkrachangtoy@gmail.com>
2021-06-23 21:21:21 -0700

    Style student list

--- student-profiles/src/components/StudentList.js
+++ student-profiles/src/components/StudentList.js
@@ -28,15 +28,17 @@ export default function StudentList() {
         return avg;
     }
     return (
-        <div>
+        <div className="student-list-container">
             {students.map(student => 
-                <div key={student.id}>
-                    <img src={student.pic} alt={student.firstName}/>
-                    <h1>{`${student.firstName} ${student.lastName}`}</h1>
-                    <p>Email: {student.email}</p>
-                    <p>Company: {student.company}</p>
-                    <p>Skill: {student.skill}</p>
-                    <p>Average: {handleAverageGrade(student.grades)}</p>
+                <div className="student-list__item" key={student.id}>
+                    <div style={{backgroundImage: `url(${student.pic})`}} className="student-list__img" />
+                    <div className="student-list__info">
+                    <h1 className="student-list__name">{`${student.firstName} ${student.lastName}`}</h1>
+                        <p className="student-list__text">Email: {student.email}</p>
+                        <p className="student-list__text">Company: {student.company}</p>
+                        <p className="student-list__text">Skill: {student.skill}</p>
+                        <p className="student-list__text">Average: {handleAverageGrade(student.grades)}%</p>
+                    </div>
                 </div>
                 )}
         </div>
