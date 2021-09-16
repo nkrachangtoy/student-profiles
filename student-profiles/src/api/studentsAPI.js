08aa905172123c8be3a9e8802b3e0a6ec72a6027
Nusorn Krachangtoy <nkrachangtoy@gmail.com>
2021-09-16 13:38:08 -0700

    prepare for deployment

--- student-profiles/src/api/studentsAPI.js
+++ student-profiles/src/api/studentsAPI.js
@@ -3,11 +3,10 @@ const API_URL = process.env.REACT_APP_BASE_URL;
 
 // Get student profiles
 export const getStudentProfiles = async () => {
-  console.log(API_URL);
   try {
     const response = await axios.get(`${API_URL}`);
     return response;
   } catch (e) {
-    console.log(e);
+    console.error(e);
   }
 };
